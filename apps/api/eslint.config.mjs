5e5cf11e1d11a54e8298fd8c385bb28ff490759f
Eder Queiroz <eder.queiroz@graodireto.com.br>
2025-09-21 18:50:52 -0300

    feat: setup monorepo with NestJS API

---
+++ apps/api/eslint.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import globals from 'globals';
+import { baseConfig } from '../../eslint.config.mts';
+
+export default [
+  ...baseConfig,
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+    },
+  },
+];
